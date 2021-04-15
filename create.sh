6cc93b9dd160ef83ffe5dc971e0f690cb59af16e
chamchenko <chamchenko@gmail.com>
2021-04-15 07:25:04 +0100

    add xtreamcodes

--- create.sh
+++ create.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+rm  -r ../tmp/* 2> /dev/null
 echo "Downloading addons .."
 curl -sL --output - https://github.com/chamchenko/plugin.video.cbsnetwork/archive/refs/heads/dev.zip | busybox unzip -q - -d ../tmp/
 curl -sL --output - https://github.com/chamchenko/xbmc.plugin.video.nba/archive/refs/heads/dev.zip | busybox unzip -q - -d ../tmp/
+curl -sL --output - https://github.com/chamchenko/plugin.video.xtreamcodes/archive/refs/heads/dev.zip | busybox unzip -q - -d ../tmp/
 echo "Creating Repository .."
 ./create_repository.py \
     --datadir ./zips/dev \
@@ -9,7 +11,8 @@ echo "Creating Repository .."
     --checksum ./xmls/dev/addons.xml.md5 \
     ./srcs/dev/ \
     ../tmp/xbmc.plugin.video.nba-dev/ \
-    ../tmp/plugin.video.cbsnetwork-dev/
+    ../tmp/plugin.video.cbsnetwork-dev/ \
+    ../tmp/plugin.video.xtreamcodes-dev
 
 echo "Removing temp files .."
 rm -r ../tmp/*
