37db3a87bdcb6a749dfe00cd0345721de459bc09
chamchenko <chamchenko@gmail.com>
2021-04-18 07:34:18 +0100

    add maraya

--- create.sh
+++ create.sh
@@ -6,6 +6,7 @@ curl -sL --output - https://github.com/chamchenko/xbmc.plugin.video.nba/archive/
 curl -sL --output - https://github.com/chamchenko/plugin.video.xtreamcodes/archive/refs/heads/dev.zip | busybox unzip -q - -d ../tmp/
 curl -sL --output - https://github.com/chamchenko/plugin.video.adtv/archive/refs/heads/dev.zip | busybox unzip -q - -d ../tmp/
 curl -sL --output - https://github.com/chamchenko/plugin.video.awaan/archive/refs/heads/dev.zip | busybox unzip -q - -d ../tmp/
+curl -sL --output - https://github.com/chamchenko/plugin.video.maraya/archive/refs/heads/dev.zip | busybox unzip -q - -d ../tmp/
 echo "Creating Repository .."
 ./create_repository.py \
     --datadir ./zips/dev \
@@ -16,6 +17,7 @@ echo "Creating Repository .."
     ../tmp/plugin.video.cbsnetwork-dev/ \
     ../tmp/plugin.video.xtreamcodes-dev \
     ../tmp/plugin.video.adtv-dev \
-    ../tmp/plugin.video.awaan-dev 
+    ../tmp/plugin.video.awaan-dev \
+    ../tmp/plugin.video.maraya-dev 
 echo "Removing temp files .."
 rm -r ../tmp/*
